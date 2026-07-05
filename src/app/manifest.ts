37747e55a8787e30eaf4fc53dbe597e1806febb9
mcastilletti <m.castilletti@gmail.com>
2026-07-05 10:16:42 +0200

    feat: add maskable PWA icon with yellow background

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#FFD500',
     icons: [
       {
-        src: '/icon.png',
+        src: '/icon-maskable.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icon.png',
+        src: '/icon-maskable.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',

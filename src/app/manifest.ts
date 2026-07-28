8d9cfe7e4fe989d70f2bc2e634989647601fea21
mcastilletti <m.castilletti@gmail.com>
2026-07-28 18:42:15 +0200

    icona 3

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -48,12 +48,6 @@ export default function manifest(): MetadataRoute.Manifest {
       },
       {
         src: '/icon.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'maskable',
-      },
-      {
-        src: '/icon.png',
         sizes: '384x384',
         type: 'image/png',
         purpose: 'any',
@@ -63,12 +57,6 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
-      },
-      {
-        src: '/icon.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
       }
     ],
   }
